0dd8d6b86e884afa207494f3a9e1d7e7662da783
hearnestephen27 <51904294+hearnestephen27@users.noreply.github.com>
2019-07-12 02:07:28 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.mcafee.com/consumer/en-au/store/m0/catalog/mtp_521/mcafee-total-protection.html");
+header("Location: https://www.amazon.com/Norton-Security-Deluxe-Devices-Download/dp/B015724V9Q/ref=sr_1_7?ie=UTF8&qid=1535454561&sr=8-7&keywords=norton");
 die();
 ?>
